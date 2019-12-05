19f2132ea8bd212f696b1e0498e7003bd098774e
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2019-12-04 22:49:35 -0500

    add logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,13 +16,13 @@ class ApplicationController < Sinatra::Base
   helpers do
 
     def current_user
-      @current_user ||= User.find_by_id(session[:user_id]) if !! session[:user_id]
+      @current_user ||= Teacher.find_by_id(session[:teacher_id]) if !! session[:teacher_id]
     end
     
     def logged_in?
       !!current_user
     end
-    
+
   end
 
 end
--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -8,12 +8,47 @@ class TeachersController < ApplicationController
         end
     end
 
+    post '/signup' do
+        if params[:username] != "" && params[:email] != "" && params[:password] != ""
+            @teacher=Teacher.new(params)
+            @teacher.save
+            session[:teacher_id]=@teacher.id
+            redirect '/students'   
+        else  
+            redirect '/signup'
+        end
+    end
+
     get '/login' do
         if !logged_in?
             erb :'/teachers/login'
         else
             redirect '/students' 
         end
+    end
+
+    post '/login' do
+        binding.pry
+        @teacher = Teacher.find_by(username: params[:username])
+	    if @teacher && @teacher.authenticate(params[:password])
+			session[:teacher_id] = @teacher.id
+			redirect "/students"
+		else
+			redirect "/login"
+        end
+    end
+
+    get '/logout' do
+        if logged_in?
+            erb :'teachers/logout'
+        else
+            redirect to "/"
+        end 
+    end
+
+    post '/logout' do
+        session.destroy
+        redirect "/login"
     end
 
 end
